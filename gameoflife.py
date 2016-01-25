1b4eaa1a5fab823c64088bf403b1e1a1b492acfd
einarpe <kubar3k+gh@gmail.com>
2016-01-25 22:06:50 +0100

    game of life

--- gameoflife.py
+++ gameoflife.py
@@ -1,17 +1,17 @@
-
+import time
 import random
 import os
 
-def norm(val, max):
-  if val == max - 1:
+def norm(val, maxVal):
+  if val == maxVal:
     return 0
-  elif val == 0:
-    return max - 1
+  elif val < 0:
+    return maxVal - 1
   else:
     return val
-  # return 0 if val == max - 1 else (max - 1 if val == 0 else val)
 
 def printArea(area):
+  _ = os.system("cls")
 
   for y in range(len(area)):
     for x in range(len(area[y])):
@@ -21,43 +21,53 @@ def printArea(area):
 
 def main():
   '''
-    Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-    Any live cell with two or three live neighbours lives on to the next generation.
-    Any live cell with more than three live neighbours dies, as if by over-population.
-    Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.'''
+      Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+      Any live cell with two or three live neighbours lives on to the next generation.
+      Any live cell with more than three live neighbours dies, as if by over-population.
+      Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.'''
+  run = True
+  while run:
+    try:
+      width, height = (80, 50)
+      area = [[random.randint(0, 1) for x in range(width)] for y in range(height)]
 
-  width, height = (10, 10)
-  area = [[random.randint(0, 1) for x in range(width)] for y in range(height)]
+      generations = 0
+      while True:
+        printArea(area)
 
-  generations = 0
-  while generations < 1:
-    printArea(area)
+        newArea = [[0 for x in range(width)] for y in range(height)]
+        for y in range(height):
+          for x in range(width):
+            current = area[y][x]
+            next = 0
 
-    newArea = [[0 for x in range(width)] for y in range(height)]
-    for y in range(height):
-      for x in range(width):
-        current = area[y][x]
-        next = 0
+            neighbours = [
+                area[norm(y - 1, height)][norm(x - 1, width)], area[norm(y - 1, height)][x], area[norm(y - 1, height)][norm(x + 1, width)],
+                area[y                  ][norm(x - 1, width)],                               area[y                  ][norm(x + 1, width)],
+                area[norm(y + 1, height)][norm(x - 1, width)], area[norm(y + 1, height)][x], area[norm(y + 1, height)][norm(x + 1, width)]
+            ]
+            
+            s = sum(neighbours)
+            
+            if current == 1:
+              next = 1 if s == 2 or s == 3 else 0
+            elif current == 0:
+              next = 1 if s == 3 else 0
 
-        neighbours = [
-            area[norm(y - 1, height)][norm(x - 1, width)], area[norm(y - 1, height)][x], area[norm(y - 1, height)][norm(x + 1, width)],
-            area[y                  ][norm(x - 1, width)],                               area[y                  ][norm(x + 1, width)],
-            area[norm(y + 1, height)][norm(x - 1, width)], area[norm(y + 1, height)][x], area[norm(y + 1, height)][norm(x + 1, width)]
-        ]
-        
-        s = sum(neighbours)
-        
-        if current == 1:
-          next = 1 if s == 2 or s == 3 else 0
-        elif current == 0:
-          next = 1 if s == 3 else 0
+            # print("c(%d,%d)=%d, n=%d" % (x, y, current, next))
+            newArea[y][x] = next
 
-        print("c(%d,%d)=%d, n=%d" % (x, y, current, next))
-        newArea[y][x] = next
+        generations += 1
+        area = newArea
 
-    generations += 1
-    area = newArea
+        time.sleep(0.1)
 
+        #ts = sum([sum(area[y]) for y in range(height)])
+        #if ts == 0:
+
+
+    except KeyboardInterrupt:
+      run = True if input("again? (y/n) ").strip().lower() == "y" else False
 
 
 if __name__ == "__main__":
